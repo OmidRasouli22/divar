6a1a0468e2da4c45413a00cb572d2e403741eedf
Omid Rasouli <omidrasouli1998@gmail.com>
2024-05-13 16:17:16 +0330

    adding template

--- src/app.routes.js
+++ src/app.routes.js
@@ -2,7 +2,12 @@ import { Router } from "express";
 const router = Router();
 
 router.get("/", (req, res) => {
-  res.render("./pages/index.ejs");
+  res.locals.layout = "./layouts/home/main.ejs";
+  res.render("./pages/home/index.ejs");
+});
+
+router.get("/panel", (req, res) => {
+  res.render("./pages/panel/dashboard.ejs");
 });
 
 export default router;
